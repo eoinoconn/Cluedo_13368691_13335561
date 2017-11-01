653cfc88d7ff355be8b6866ff3f51863ffc8a6c1
eoinoconn <19649582+eoinoconn@users.noreply.github.com>
2017-11-01 17:09:57 +0000

    Added room enum type, added...
     added room cards to initialisation, removed gameboard initialisation for the moment, main asks players for names and randomly assigns characters for the moment, hopping to get the self assign down at a later date, main creates player instances.

--- src/ie/ucd/items/Cluedo.java
+++ src/ie/ucd/items/Cluedo.java
@@ -2,46 +2,69 @@ package ie.ucd.items;
 
 import java.util.Hashtable;
 import java.util.Scanner;
-import java.util.Arrays;
-import java.io.*;
+import java.util.ArrayList;
+
 
 public class Cluedo {
 	public static void main(String [] args) throws Exception {
 		
-		// Create instances of game components
-		// TODO This could be replaced at a later date with a facade
-		// TODO merge into one Hashtable
-		Hashtable<Person, Card> personCardDeck = new Hashtable<Person, Card>();
+		
+		// TODO Initialise gameboard
+		// TODO randomly assign weapon pawns
+		int[] location = {1,1}; //test variable
+		
+		
+		// TODO This could be replaced at a later date with a facade		
+		// Create instances of game cards
+		// Store game cards in cardDeck
+		ArrayList<Card> cardDeck = new ArrayList<Card>();
+		ArrayList<Person> personCollection = new ArrayList<Person>();
 		for (Person per : Person.values())
 		{
-			personCardDeck.put(per, new PersonCard(per));
+			cardDeck.add(new PersonCard(per));
+			personCollection.add(per);
 		}
-		Hashtable<Weapon, Card> weaponCardDeck = new Hashtable<Weapon, Card>();
 		for (Weapon wep : Weapon.values())
 		{
-			weaponCardDeck.put(wep, new WeaponCard(wep));
+			cardDeck.add(new WeaponCard(wep));
+		}
+		for (Room ro : Room.values())
+		{
+			cardDeck.add(new RoomCard());
 		}
 		
-		// Read grid from file
-		String fileName = "grid.txt";
-		File csv = new File(fileName);
-		double[][] grid = new double[10][10];
-        String[] testStr = new String[30];
-        int[] results = new int[10];
-        
-        String delimiter = ",";
-        
-        // TODO error catching
-        Scanner sc = new Scanner(csv);
-        
-        while(sc.hasNextLine()) {
-        	String line = sc.nextLine();
-        	testStr = line.split(delimiter);
-        	for(int i=0;i<testStr.length;i++) {
-        		results[i] = Integer.parseInt(testStr[i]);
-        	}
-        	
-        }
-        sc.close();
+		
+
+
+		
+		System.out.println("Welcome to Cluedo!! By eoin and Andy.");
+		
+		// Establish who's playing
+		Scanner sc = new Scanner(System.in);
+		boolean anotherPlayer = false;
+		Hashtable<String, Player> playerCollection = new Hashtable<String, Player>();
+		int players = 0;
+		
+		// Loop to repeatedly ask for new players
+		do {
+			
+			// Input player name and initialise player object
+			System.out.println("Hello player " + (++players) + '.');
+			System.out.println("Please enter your name:");
+			// TODO catch exceptions
+			String str = sc.next();
+			playerCollection.put(str, new Player()); // personCollection.get(players)));
+			System.out.println("Welcome to the game " + str + '!');
+		
+			
+			// Check if other players are present
+			System.out.println("Would you like to add another player? (Y/N)");
+			str = sc.nextLine();
+			if (Character.toUpperCase(str.charAt(0)) == 'Y') {
+				anotherPlayer = true;
+			} else anotherPlayer = false;
+			
+		} while (anotherPlayer);
+		sc.close();
 	}
 }
