ca1c4125705932f2de2dd6febafdf6e621a351a5
andycsyou <andrew.cullen@ucdconnect.ie>
2017-11-04 17:28:15 +0000

    fixed bug and cleaned up checks for out of bounds cells

--- src/ie/ucd/items/GameBoard.java
+++ src/ie/ucd/items/GameBoard.java
@@ -16,25 +16,26 @@ public class GameBoard {
 		int[] location = pawn.getLocation();
 		int[] options = new int[5]; // some array of numbers representing options
 		options[0] = grid[location[0]][location[1]]; //current position
-		if((location[1]-1)>=0 && (location[1]-1)<DIMENSIONS) {
-			options[1] = grid[location[0]][location[1]-1]; //cell above
+		if((location[1]-1)>=0) { // ensure space above is not out of bounds
+			options[1] = grid[location[0]][location[1]-1];
 		}
-		if((location[1]+1)>=0 && (location[1]+1)<DIMENSIONS) {
-			options[2] = grid[location[0]][location[1]+1]; //cell below
+		if((location[1]+1)<DIMENSIONS) { // ensure space below is not out of bounds
+			options[2] = grid[location[0]][location[1]+1];
 		}
-		if((location[0]-1)>=0 && (location[0]-1)<DIMENSIONS) {
-			options[3] = grid[location[0]-1][location[1]]; //cell left
+		if((location[0]-1)>=0) { // ensure space to the left is not out of bounds
+			options[3] = grid[location[0]-1][location[1]];
 		}
-		if((location[0]+1)>=0 && (location[0]+1)<DIMENSIONS) {
-			options[4] = grid[location[0]+1][location[1]]; //cell right
+		if((location[0]+1)<DIMENSIONS) { // ensure space to the right is not out of bounds
+			options[4] = grid[location[0]+1][location[1]];
 		}
 		return options;
 	}
 	
 	public int[] getRoomLocation(Room room) {
 		int[] location = new int[2];
-		for (int i = 0; i < DIMENSIONS; i++) {
-	        for (int j = 0; j < DIMENSIONS; j++) {
+		// check only the space not adjacent to the edge of the board as there are never doors
+		for (int i = 1; i < DIMENSIONS-1; i++) {
+	        for (int j = 1; j < DIMENSIONS-1; j++) {
 	            if (grid[i][j]==room.ordinal()+10 && (grid[i+1][j]==room.ordinal() || grid[i-1][j]==room.ordinal() || grid[i][j+1]==room.ordinal() || grid[i][j-1]==room.ordinal())) {
 	                // if room in room adjacent to doorway
 	            	location[0] = i;
@@ -43,7 +44,7 @@ public class GameBoard {
 	            }
 	        }
 		}
-		return location; // will return -1, 0 as location if room number not found in grid
+		return location; // will return first grid square if no door found
 		
 	}
 }
