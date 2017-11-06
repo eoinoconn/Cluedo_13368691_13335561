20caa0b9e8cdba1b3a49e60a23df4d33877017af
andycsyou <andrew.cullen@ucdconnect.ie>
2017-11-06 11:15:03 +0000

    Move movement to Turn class and change return type to int. Getter for suspectPawn in Player class

--- src/ie/ucd/items/Player.java
+++ src/ie/ucd/items/Player.java
@@ -1,7 +1,5 @@
 package ie.ucd.items;
 
-import java.util.Scanner;
-
 public class Player {
 	
 	private SuspectPawn suspectPawn;
@@ -12,66 +10,16 @@ public class Player {
 		moves = 0;
 	}
 	
+	public SuspectPawn getSuspectPawn() {
+		return suspectPawn;
+	}
+	
 	public int getMoves() {
 		return moves;
 	}
 	
 	public void setMoves(int moves) {
 		this.moves = moves;
-	}
-	
-	
-	
-	public boolean makeMove(GameBoard board) {
-		int[] options = board.getOptions(suspectPawn);
-		System.out.println("\t" + options[1] + "\n" + options[3] + "\t" + options[0] + "\t" + options[4] +"\n" + "\t" + options[2]);
-		System.out.println("Select Direction: 'u' for up, 'd' for down, 'l' for left, 'r' for right");
-		
-		Scanner sd = new Scanner(System.in);
-		String movement = sd.nextLine();
-		sd.close();
-		switch(Character.toLowerCase(movement.charAt(0))) {
-		case 'u':	
-			if(options[1]>0) {
-				suspectPawn.setLocation(suspectPawn.getLocation()[0], suspectPawn.getLocation()[1]-1);
-				if(options[1]<10) { // do not decrement moves if in a room
-					this.moves--;
-				}
-				return true;
-			}
-			return false;
-		case 'd':
-			if(options[2]>0) {
-				suspectPawn.setLocation(suspectPawn.getLocation()[0], suspectPawn.getLocation()[1]+1);
-				if(options[2]<10) { // do not decrement moves if in a room
-					this.moves--;
-				}
-				return true;
-			}
-			return false;
-		case 'l':
-			if(options[3]>0) {
-				suspectPawn.setLocation(suspectPawn.getLocation()[0]-1, suspectPawn.getLocation()[1]);
-				if(options[3]<10) { // do not decrement moves if in a room
-					this.moves--;
-				}
-				return true;
-			}
-			return false;
-		case 'r':
-			if(options[4]>0) {
-				suspectPawn.setLocation(suspectPawn.getLocation()[0]+1, suspectPawn.getLocation()[1]);
-				if(options[4]<10) { // do not decrement moves if in a room
-					this.moves--;
-				}
-				return true;
-			}
-			return false;
-		default:
-			return false;
-				
-		}
-
 	}
 	
 }
