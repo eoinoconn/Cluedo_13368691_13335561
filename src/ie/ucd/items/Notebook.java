d8ee3af5badb97e0a7f525733cfb1838ede0bb7c
Unknown <eoinoconn@gmail.com>
2017-11-10 18:23:05 +0000

    Finished notebook class
    notebook class now has two methods, addEvent and lastNEntries

--- src/ie/ucd/items/Notebook.java
+++ src/ie/ucd/items/Notebook.java
@@ -5,34 +5,27 @@ import java.util.ArrayList;
 public class Notebook {
 
 	
-	private static ArrayList<String> refutedHypothesis;
-	private static ArrayList<String> playerLocations;
-	private ArrayList<String> notGuilty;
-	
+	private ArrayList<String> events;
+	private int size;
 	
 	public Notebook() {
-		
-	}
-	/*
-	 * Player 2 refuted player 1s hypothesis of x, y z
-	 */
-	public void addRefutedHypothesis(){		//TODO takes item when in effect
-		
+		size = 0;
 	}
 	
-	/*
-	 * Build grid as per: http://blog.clearvisionstudios.net/wp-content/uploads/2012/10/Clue-Detective-Notebook-sml.jpg
-	 */
-	public void notGuilty() {
-		
+	public void addEvent(String event) {
+		events.add(event);
+		size++;
 	}
-	
 	
 	/*
 	 * 
 	 */
-	public void addPlayerLocation(Suspect Name, int x_loc, int y_loc) {
-		
+	public String lastNEntries(int N) {
+		String str = "";
+		for (int i = (size - N); i < size; i++) {
+			str += (events.get(i) + "\n");
+		}
+		return str;
 	}
 	
 	
