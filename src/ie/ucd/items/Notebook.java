21ba5d5c557f277a0d9b21649bf8f989917f4d00
Unknown <eoinoconn@gmail.com>
2017-11-10 17:04:23 +0000

    Added notebook class

---
+++ src/ie/ucd/items/Notebook.java
@@ -0,0 +1,39 @@
+package ie.ucd.items;
+
+import java.util.ArrayList;
+
+public class Notebook {
+
+	
+	private static ArrayList<String> refutedHypothesis;
+	private static ArrayList<String> playerLocations;
+	private ArrayList<String> notGuilty;
+	
+	
+	public Notebook() {
+		
+	}
+	/*
+	 * Player 2 refuted player 1s hypothesis of x, y z
+	 */
+	public void addRefutedHypothesis(){		//TODO takes item when in effect
+		
+	}
+	
+	/*
+	 * Build grid as per: http://blog.clearvisionstudios.net/wp-content/uploads/2012/10/Clue-Detective-Notebook-sml.jpg
+	 */
+	public void notGuilty() {
+		
+	}
+	
+	
+	/*
+	 * 
+	 */
+	public void addPlayerLocation(Suspect Name, int x_loc, int y_loc) {
+		
+	}
+	
+	
+}
