eaf1f9e4b52f198dcc5284ac29bddbc1c4ecaefd
Eoin <eoinoconn@gmail.com>
2017-12-22 17:09:00 +0000

    Added RoomCardTest

---
+++ src/ie/ucd/gameEntities/testCases/RoomCardTest.java
@@ -0,0 +1,33 @@
+package ie.ucd.gameEntities.testCases;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import ie.ucd.gameEntities.Room;
+import ie.ucd.gameEntities.RoomCard;
+
+class RoomCardTest {
+
+	RoomCard roomCard;
+	
+	@BeforeEach
+	void setUp() throws Exception {
+		roomCard = new RoomCard(Room.values()[0]);
+	}
+
+	@Test
+	void testToString() {
+		String str = roomCard.toString();
+		String expected = "THEBAR";
+		assertEquals(expected, str);
+	}
+	
+	@Test
+	void testGetName() {
+		assertEquals(Room.values()[0], roomCard.getName());
+	}
+}
